15e11bd43cd8b996ce82ce0ba2cebfd45dafda56
manami0624 <manana624@yahoo.co.jp>
2023-06-29 19:23:08 +0900

    言語切り替えボタン追加

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ window.addEventListener('load', function() {
   // ページロード時に初期状態を設定
   if (pullDownParents.style.display === "block") {
     menuText.innerHTML = `
-      <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="currentColor" class="bi bi-list" viewBox="0 0 16 16">
+      <svg xmlns="http://www.w3.org/2000/svg" width="45" height="45" fill="currentColor" class="bi bi-list" viewBox="0 0 16 16">
         <path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"/>
       </svg>
     `;
@@ -52,3 +52,25 @@ window.addEventListener('load', function() {
     topImage.style.opacity = '0.9';
   }, 500); // 0.5秒後にアニメーションを開始する
 });
+
+function changeLanguage(language) {
+  if (language === 'kr') {
+    alert('한국어로 표시합니다');
+    window.location.href = 'kr.html';
+  } else if (language === 'jp') {
+    alert('ページを日本語で表示します');
+    window.location.href = 'index.html';
+  }
+}
+
+
+function showLanguageText(element) {
+  const languageText = element.querySelector("#languageText");
+  languageText.style.display = "block";
+}
+
+function hideLanguageText(element) {
+  const languageText = element.querySelector("#languageText");
+  languageText.style.display = "none";
+}
+
