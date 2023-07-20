8852b32aa36eeca8ea0c02572641ce3772539269
manami0624 <manana624@yahoo.co.jp>
2023-07-20 14:54:06 +0900

    ヘッダー調整

--- index.js
+++ index.js
@@ -24,6 +24,7 @@ window.addEventListener('load', function() {
     } else {
       pullDownParents.style.display = "block";
       menuText.textContent = "×";
+      menuText.style.fontSize = "30px";
     }
 
     // ×マークの表示と非表示の切り替え
